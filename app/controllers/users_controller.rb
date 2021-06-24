39346ef3220d6257361ec67c50b31d1c4aae2cb5
rino.takemura <floweringskyruin@gmail.com>
2021-06-24 07:25:46 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order(id: :desc).page(params[:page])
+    counts(@user)
   end
 
   def new
