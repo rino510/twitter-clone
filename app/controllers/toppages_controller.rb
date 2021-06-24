a57178a9f61af489f1025e1e903ab358b549e71e
rino.takemura <floweringskyruin@gmail.com>
2021-06-24 09:24:49 +0000

    timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,8 +1,21 @@
 class ToppagesController < ApplicationController
   def index
     if logged_in?
-      @micropost = current_user.microposts.build  # form_with 用
-      @microposts = current_user.microposts.order(id: :desc).page(params[:page])
+      @micropost = current_user.microposts.build
+      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
     end
   end
+  
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = 'メッセージを投稿しました。'
+      redirect_to root_url
+    else
+      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      render 'toppages/index'
+    end
+  end
+  
 end
